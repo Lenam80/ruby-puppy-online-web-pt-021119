84c639c89427c37560763ea018fed2c067d33519
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-18 18:07:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,20 @@
 class Dog
 
+@@all = []
+
   attr_accessor :name
 
-  def initialize (name)
+  def initialize(name)
     @name = name
+    @@all << self
   end
 
-  
+def self.clear_all
+@@all.clear
+  end
+
+def self.all
+@@all.each do
+  end
+end
+  end
